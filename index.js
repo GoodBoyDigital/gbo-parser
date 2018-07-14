13acf5bce771a4e4eb3b931fef29ce56970b7b32
Mat Groves <mat@goodboydigital.com>
2018-07-14 16:54:43 +0100

    added obj to gbo maker

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 module.exports = {
 	convertObjToGbo:require('./lib/convert-obj-to-gbo.js'),
+	convertObjsToGboBatch:require('./lib/convert-objs-to-gbo-batch.js'),
 	readGbo:require('./lib/read-gbo.js')
 }
